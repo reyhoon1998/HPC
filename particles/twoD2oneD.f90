4a1a61efafa23b414f51292768ab4eb4d2b42d30
Reyhaneh Bahranifard <123812117+reyhoon1998@users.noreply.github.com>
2026-02-19 13:14:40 -0500

    Enhance comments in twoD2oneD.f90
    Add comments explaining the 2D to 1D mapping logic.

--- particles/twoD2oneD.f90
+++ particles/twoD2oneD.f90
@@ -1,3 +1,6 @@
+! Each row in the 2D box contains 'box_size' cells.
+! Multiplying the row index 'y' by 'box_size' shifts to the correct row,
+! and adding 'x' moves to the correct column, giving a unique 1D ID.
 
 module griding
 contains
